af22f5a500a055795a9ce937e733232a082ed74b
zhning12 <zhning12@163.com>
2017-11-02 18:40:53 +0800

    ad2

--- client/routes/web/site.js
+++ client/routes/web/site.js
@@ -2,15 +2,15 @@ module.exports = function (app) {
 
 //默认根目录：/app/views
   app.get('/', function (req, res) {
-    res.render("index")
+    res.render("login")
   });
 
   app.get('/test', function (req, res) {
     res.render("test")
   });
 
-  app.get('/test2',function(req,res){
-    res.render("test2")
+  app.get('/sign_up',function(req,res){
+    res.render("sign_up")
   });
 };
 
