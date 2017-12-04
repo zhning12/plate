1d17cb66edde5a4b1b25a4dfaa10f0c9f9ff8856
guanpengchn <875499765@qq.com>
2017-12-04 19:52:09 +0800

    add calendar

--- client/app/static/scripts/calendar.js
+++ client/app/static/scripts/calendar.js
@@ -2,4 +2,87 @@ $(document).ready(function () {
 	console.log(1);
 	$(".calendar").addClass("active");
 
+
+	$('#calendar').fullCalendar({
+		header: {
+			left: 'prev,next',
+			center: 'title',
+			right: 'month,listMonth'
+		},
+		defaultDate: '2017-11-12',
+		navLinks: true, // can click day/week names to navigate views
+		//editable: true,
+		eventLimit: true, // allow "more" link when too many events
+		events: [
+			{
+				title: 'All Day Event',
+				start: '2017-11-01'
+			},
+			{
+				title: 'Long Event',
+				start: '2017-11-07',
+				end: '2017-11-10'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2017-11-09T16:00:00',
+				color: '#257e4a'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2017-11-16T16:00:00',
+				color: '#ff9f89'
+			},
+			{
+				title: 'Conference',
+				start: '2017-11-11',
+				end: '2017-11-13'
+			},
+			{
+				title: 'Meeting',
+				start: '2017-11-12T10:30:00',
+				end: '2017-11-12T12:30:00',
+				color: '#ff9f89'
+			},
+			{
+				title: 'Lunch',
+				start: '2017-11-12T12:00:00'
+			},
+			{
+				title: 'Meeting',
+				start: '2017-11-12T14:30:00'
+			},
+			{
+				title: 'Happy Hour',
+				start: '2017-11-12T17:30:00'
+			},
+			{
+				title: 'Dinner',
+				start: '2017-11-12T20:00:00'
+			},
+			{
+				title: 'Birthday Party',
+				start: '2017-11-13T07:00:00'
+			},
+			{
+				title: 'Click for Google',
+				url: 'http://google.com/',
+				start: '2017-11-28'
+			},
+			{
+				title: 'Click for Google',
+				url: 'http://google.com/',
+				start: '2017-12-02'
+			},
+			{
+				title: 'Click for Google',
+				url: 'http://google.com/',
+				start: '2017-12-04'
+			}
+		]
+	});
+
+	//$(".fc-center").css('display', 'none');
 });
--- client/app/static/scripts/person.js
+++ client/app/static/scripts/person.js
@@ -2,53 +2,58 @@
 
 $(document).ready(function () {
 	console.log(1);
+	$(".container").css("display","none");
 	$(".person").addClass("active");
 	$.ajax({
-	type: "get",
-	url: globalurl+"getSendTask"+fail,
-	dataType: 'json',
-	success: function (data) {
-		if(data['status']==1){
-			console.log(data);
-			for(var i = 0; i<data['data'].length;i++){
-				var object = data['data'][i];
-				var icon;
-				icon = object['finished']==1?'check_circle':'help_outline';
-				var card_new ='<a href="/task/1">'+
-				'<div class="todo break-word">'+
-					'<div class="name ">'+
-						'<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-'+object['id']+'">'+
-							'<input type="checkbox" id="checkbox-'+object['id']+'"class="mdl-checkbox__input">'+
-							'<span class="mdl-checkbox__label todo-text">'+object['name']+'</span>'+
-						'</label>'+
-					'</div>'+
-					'<div class="info truncate-text">'+
-						'<span class="asignee">'+
-							'<img src="" class="avater">'+object['leader']+
-						'</span>'+
-						'<span class="due-at">'+
-							'<i class="material-icons">schedule</i>'+
-							object['deadline']+
-						'</span>'+
-					'</div>'+
-				'</div>'+
-			'</a>';
-				$("#tab1").append(card_new);
-			}				
-			var card_none = '<div class="card-none"></div>';
-			for(var i =0;i<3;i++){
-				$(".container").append(card_none);
+		type: "get",
+		url: globalurl + "getSendTask" + fail,
+		dataType: 'json',
+		async:false,
+		success: function (data) {
+			if (data['status'] == 1) {
+				console.log(data);
+				for (var i = 0; i < data['data'].length; i++) {
+					var object = data['data'][i];
+					var icon;
+					icon = object['finished'] == 1 ? 'check_circle' : 'help_outline';
+					var card_new = '<a href="/task/1">' +
+						'<div class="todo break-word">' +
+						'<div class="name ">' +
+						'<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-' + object['id'] + '">' +
+						'<input type="checkbox" id="checkbox-' + object['id'] + '"class="mdl-checkbox__input">' +
+						'<span class="mdl-checkbox__label todo-text">' + object['name'] + '</span>' +
+						'</label>' +
+						'</div>' +
+						'<div class="info truncate-text">' +
+						'<span class="asignee">' +
+						'<img src="" class="avater">' + object['leader'] +
+						'</span>' +
+						'<span class="due-at">' +
+						'<i class="material-icons">schedule</i>' +
+						object['deadline'] +
+						'</span>' +
+						'</div>' +
+						'</div>' +
+						'</a>';
+					$("#tab1").append(card_new);
+				}
+				var card_none = '<div class="card-none"></div>';
+				for (var i = 0; i < 3; i++) {
+					$(".container").append(card_none);
+				}
 			}
+			else {
+				alert('error!');
+			}
+
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+			console.log(textStatus);
+			console.log(errorThrown);
 		}
-		else{
-			alert('error!');
-		}
-		
-	},
-	error: function (jqXHR, textStatus, errorThrown) {
-		console.log(jqXHR);
-		console.log(textStatus);
-		console.log(errorThrown);
-	}
-});
+	});
+	$("#progress").css("display","none");
+	$(".container").css("display","flex");
+	
 });
