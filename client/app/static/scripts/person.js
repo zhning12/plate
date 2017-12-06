743db6560f1c97cec35d6eb6e8f2953d59bbda2b
Phoebe <zhning12@163.com>
2017-12-06 10:26:08 +0800

    add choiceall

--- client/app/static/scripts/person.js
+++ client/app/static/scripts/person.js
@@ -2,9 +2,33 @@ $(document).ready(function () {
 	console.log(1);
 	$(".container").css("display", "none");
 	$(".person").addClass("active");
+	$.ajax({
+		type: "get",
+		url: globalurl + "getUser" + fail,
+		dataType: 'json',
+		success: function (data) {
+			if (data['status'] == 1) {
+				console.log(data);
+				var username = data['data']['username'];
+				$("#user_name").append(username);
+				var email=data['data']['email']
+				$("#e-mail").append(email);
+				var avatar=data['data']['avatar']
+				document.getElementById("avatar").src=avatar;
+			}
+			else {
+				alert('error!');
+			}
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+			console.log(textStatus);
+			console.log(errorThrown);
+		}
+	});
 	var objects1 = [];
 	var objects2 = [];
-	$.ajax({
+ 	$.ajax({
 		type: "get",
 		url: globalurl + "getSendTask" + fail,
 		dataType: 'json',
@@ -12,12 +36,37 @@ $(document).ready(function () {
 		success: function (data) {
 			if (data['status'] == 1) {
 				console.log(data);
-				var objects1=data['data'];
 				for(var i=0;i<data['data'].length;i++){
 					object = data['data'][i];
-					checkbox= "checkbox_"+object['id'];
+					checkbox= "checkbox_s"+object['id'];
 					object['checkbox_id']=checkbox;
 					objects1[i]=object;
+				}
+			}
+			else {
+				alert('error!');
+			}
+
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+			console.log(textStatus);
+			console.log(errorThrown);
+		}
+	});
+	$.ajax({
+		type: "get",
+		url: globalurl + "getReceiveTask" + fail,
+		dataType: 'json',
+		async: false,
+		success: function (data) {
+			if (data['status'] == 1) {
+				console.log(data);
+				for(var i=0;i<data['data'].length;i++){
+					object = data['data'][i];
+					checkbox= "checkbox_r"+object['id'];
+					object['checkbox_id']=checkbox;
+					objects2[i]=object;
 				}
 			}
 			else {
