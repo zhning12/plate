6521c43d5f3660a61425fd9555d09f2fa8a7225c
guanengchn <875499765@qq.com>
2017-12-13 23:48:37 +0800

    fix

--- client/app/static/scripts/switch.js
+++ client/app/static/scripts/switch.js
@@ -1,6 +1,41 @@
+
 $(document).ready(function () {
-
+	var count = 0;
+	var ajax_num = 1;
 	$(".more").addClass("active");
-
-	display();
+	var card_new = new Vue({
+		el: '#card_list',
+		data: {
+			objects: []
+		},
+		created: function () {
+			$.ajax({
+				type: "get",
+				url: globalurl + "getTeam" + fail,
+				xhrFields: { withCredentials: true },
+				crossDomain: true,
+				dataType: 'json',
+				success: function (data) {
+					if (data['status'] == 1) {
+						console.log(data);
+						for (var i = 0; i < data['data'].length; i++) {
+							var object = data['data'][i];
+							var icon;
+							icon = object.finished == 1 ? 'check_circle' : 'help_outline';
+							object['icon'] = icon;
+							Vue.set(card_new.objects, i, object);
+							card_new.objects.splice(i, 1, object);
+						}
+					}
+					else {
+						console.log(data);
+						//alert('error!');
+					}
+					display(++count, ajax_num);
+				},
+				error: ajaxError
+			});
+		},
+		delimiters: ['${', '}']
+	})
 });
