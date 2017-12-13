7e1478051341e45822defc2e7c514e4727a7c3e6
Phoebe <zhning12@163.com>
2017-12-13 22:41:22 +0800

    correct sign color

--- client/app/static/scripts/more.js
+++ client/app/static/scripts/more.js
@@ -1,24 +1,8 @@
 $(document).ready(function () {
-    var count=0;
-    var ajax_num=1;
-	console.log(1);
-	$(".more").addClass("active");
-	$.ajax({
-        type: "get",
-        url: globalurl+"getUser"+fail,
-        dataType: 'json',
-        success: function (data) {
-            if(data['status']==1){
-                console.log(data);
-                var teamName = data['data']['teamName'];  
-                $("#teamName").append(teamName);
-            }
-            else{
-                alert('error!');
-            } 
-            display(++count,ajax_num);
-        },
-        error: ajaxError
-    });
+
+    $(".more").addClass("active");
+    var teamName = $.cookie('teamName');  
+    $("#teamName").append(teamName);
+	display();
 	
 });
