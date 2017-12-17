1858eed8c36de7af8764496a01d57ea73f70ea24
Phoebe <zhning12@163.com>
2017-12-17 16:24:48 +0800

    detail mobile

--- client/app/static/scripts/detail.js
+++ client/app/static/scripts/detail.js
@@ -3,82 +3,104 @@ $(document).ready(function () {
 	var ajax_num = 1;
 	var task = [];
 	$(".task").addClass("active");
-	var task_id=getQueryVariable();
+	var task_id = getQueryVariable();
 	console.log(task_id);
-	$.ajax({
-		type: "get",
-		url: globalurl + "getTask/"+task_id + fail,
-		xhrFields: {withCredentials: true},
-		crossDomain: true,
-		dataType: 'json',
-		async: false,
-		success: function (data) {
-			if (data['status'] == 1) {
-				data=data['data'];
-				console.log(data);
-				$("#task_name").append(data['name']);
-				if(data['finished']==0){
-					$('#task_title').prop('checked',false);
-				}
-				else{
-					$('#task_title').prop('checked',true);
-				}
-				
-				if(data['description']){
-					$("#detail_info").append(data['description']);
-				}
-                else{
-					$('.task_detail').addClass('none');
-				}
-			}
-			else {
-				alert('error!');
-			}
-			display(++count,ajax_num);
-		},
-		error: ajaxError
-	});
-	var task_name = new Vue({
-		el: '#task_name',
+	var members = [];
+	var task_detail = new Vue({
+		el: '#contain',
 		data: {
-			task: "task"
+			members: []
 		},
+		created: function () {
+			$.ajax({
+				type: "get",
+				url: globalurl + "getTask/" + task_id + fail,
+				xhrFields: { withCredentials: true },
+				crossDomain: true,
+				dataType: 'json',
+				success: function (data) {
+					if (data['status'] == 1) {
+						data = data['data'];
+						console.log(data);
+						//显示任务名字
+						$("#task_name").append(data['name']);
+						//根据任务是否完成给框框打勾
+						var state = (data['finished'] == 0) ? false : true;
+						$('#task_title').prop('checked', state);
+
+						//是否有详情描述
+						if (data['description']) {
+							$("#detail_info").append(data['description']);
+						}
+						else {
+							$('.task_detail').addClass('none');
+						}
+						//截止时间
+						$('#deadline').append(data['deadline']);
+						//获取成员信息
+						task_detail.members = data['member'];
+						console.log(task_detail.members);
+						//图片
+						if (data['addedUrl']=='') {
+
+							$('.task_file').addClass('none');
+						}
+						else {
+							var urls = data['addedUrl'];
+							console.log(urls);
+							for (var index in urls) {
+								var ele = '<img class="materialboxed" data-caption="" src="/upload/' + urls[index] + '"></div>';
+								$('.thumbnails').append(ele);
+							}
+							$('.materialboxed').materialbox();
+						}
+
+					}
+					else {
+						Materialize.toast('读取任务失败', 4000);
+					}
+					display(++count, ajax_num);
+				},
+				error: ajaxError
+			});
+		},
+
 		delimiters: ['${', '}']
 	});
-	
-	$('#task_title').change(function(){
-		var finished=$('#task_title').prop('checked');
-		finished=finished?'1':'0';
+
+
+	$('#task_title').change(function () {
+		var finished = $('#task_title').prop('checked');
+		finished = finished ? '1' : '0';
 		console.log(finished);
 		var jsonData = {
-            "taskId": task_id,
-            "finished": finished
-        };
-        console.log(jsonData);
-        $.ajax({
-            type: "post",
+			"taskId": task_id,
+			"finished": finished
+		};
+		console.log(jsonData);
+		$.ajax({
+			type: "post",
 			url: globalurl + "updateState",
-			xhrFields: {withCredentials: true},
+			xhrFields: { withCredentials: true },
 			crossDomain: true,
-            dataType: 'json',
-            data: jsonData,
-            success:function(data){
-				if(data['status']==1){
+			dataType: 'json',
+			data: jsonData,
+			success: function (data) {
+				if (data['status'] == 1) {
 					console.log('success');
 				}
-				else{
+				else {
 					console.log('failed');
 				}
 			},
-            error: ajaxError
-        });
-		
+			error: ajaxError
+		});
+
 	});
 });
 
-function getQueryVariable()
-{
-       var query = window.location['pathname'];
-	   var vars = query.split("/");
-       return vars.pop();
+function getQueryVariable() {
+	var query = window.location['pathname'];
+	var vars = query.split("/");
+	return vars.pop();
 }
--- client/app/static/scripts/global.js
+++ client/app/static/scripts/global.js
@@ -1,6 +1,6 @@
 
-var globalurl = "http://39.106.147.86:7000/"
-//var globalurl = "http://10.10.155.45:7000/"
+//var globalurl = "http://39.106.147.86:7000/"
+var globalurl = "http://10.10.155.102:7000/"
 //var globalurl = "http://localhost:7000/"
 //var globalurl = "http://result.eolinker.com/n7d6z5u6691b12046479482492142f3acfa7b501e22b581?uri=http://localhost:7000/";
 
